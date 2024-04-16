d29b1f89ed316f452ed86d851a2772d40ec0c76d
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2024-04-16 17:21:12 +0300

    feat: routing metadata

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,7 +1,24 @@
-export default function ProductDetails({
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+export const generateMetadata = async ({
   params,
-}: {
-  params: { productId: string };
-}) {
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default function ProductDetails({ params }: Props) {
   return <h1>Details about product {params.productId}</h1>;
 }
