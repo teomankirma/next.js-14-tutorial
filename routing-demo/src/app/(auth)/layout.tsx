f8e954a509f8d610f13cbd6c7c69ac8d5ec73f98
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2024-04-16 18:41:00 +0300

    feat: active links

---
+++ routing-demo/src/app/(auth)/layout.tsx
@@ -0,0 +1,37 @@
+"use client";
+import "./styles.css";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot Password", href: "/forgot-password" },
+];
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+
+  return (
+    <div>
+      {navLinks.map((link) => {
+        const isActive = pathname.startsWith(link.href);
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+      {children}
+    </div>
+  );
+}
