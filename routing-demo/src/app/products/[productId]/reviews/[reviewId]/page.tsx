e91036ff73e216505e36135754c06f9764447c29
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2024-04-17 13:53:47 +0300

    feat: error handling

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,9 @@
 import { notFound } from "next/navigation";
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default function ReviewDetail({
   params,
 }: {
@@ -8,6 +12,12 @@ export default function ReviewDetail({
     reviewId: string;
   };
 }) {
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error("Error loading review");
+  }
+
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
