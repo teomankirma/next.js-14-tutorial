c963bc55377dc61986dab346172d54f5aef8f794
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2024-04-18 18:32:00 +0300

    feat: headers in route handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,16 @@
-export async function GET() {
-  return new Response("Profile API data");
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  const headerList = headers();
+
+  console.log(requestHeaders.get("Authorization"));
+  console.log(headerList.get("Authorization"));
+
+  return new Response("<h1>Profile API data</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
