df647a7eb12eabf0153815f1f1b817167f47c79b
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2024-04-20 18:22:11 +0300

    feat: client component placement

---
+++ rendering-demo/src/components/nav-search.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useState } from "react";
+
+export const NavSearch = () => {
+  console.log("NavSearch rendered");
+
+  const [search, setSearch] = useState("");
+  return <div>Nav search input</div>;
+};
---
+++ rendering-demo/src/components/navbar.tsx
@@ -0,0 +1,13 @@
+import { NavLinks } from "./nav-links";
+import { NavSearch } from "./nav-search";
+
+export const Navbar = () => {
+  console.log("Navbar rendered");
+
+  return (
+    <div>
+      <NavLinks />
+      <NavSearch />
+    </div>
+  );
+};
