698ff3bfd7c69bc1f4c678758589086cb46a9d49
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2024-04-20 18:12:20 +0300

    feat: client-only code

--- rendering-demo/src/app/server-route/page.tsx
+++ rendering-demo/src/app/server-route/page.tsx
@@ -1,9 +1,11 @@
 import ImageSlider from "@/components/ImageSlider";
+import { clientSideFunction } from "@/utils/client-utils";
 import { serverSideFunction } from "@/utils/server-utils";
 
 export default function ServerRoutePage() {
   console.log("Server route rendered");
   const result = serverSideFunction();
+  const clientResult = clientSideFunction();
   return (
     <>
       <h1>ServerRoutePage</h1>
