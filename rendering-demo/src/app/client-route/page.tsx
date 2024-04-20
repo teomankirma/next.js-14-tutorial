ef6a6ab1a4177668d17b3a2266f21de3e937cfe0
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2024-04-20 17:33:07 +0300

    feat: context providers

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -4,27 +4,13 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useTheme } from "@/components/theme-provider";
 
 export default function ClientRoutePage() {
+  const theme = useTheme();
+
   const settings = {
     dots: true,
   };
-  return (
-    <div className="image-slider-container">
-      <Slider {...settings}>
-        <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-        <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-        <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-        <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-      </Slider>
-    </div>
-  );
+  return <h1 style={{ color: theme.colors.secondary }}>Client route</h1>;
 }
